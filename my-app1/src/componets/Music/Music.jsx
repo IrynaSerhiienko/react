f95fdcd81ed3b7e2c6aebe4e7a2d2636567e3583
Iryna Serhiienko <irishka.sergienko@gmail.com>
2023-02-28 23:44:44 +0200

    Test

--- my-app1/src/componets/Music/Music.jsx
+++ my-app1/src/componets/Music/Music.jsx
@@ -4,6 +4,11 @@ import s from './Music.module.css';
 const Music = (props) => {
     return ( <div className={s.music}>
         Music
+        Music
+        Music
+        Music
+        Music
+        Music
     </div>
     )
 }

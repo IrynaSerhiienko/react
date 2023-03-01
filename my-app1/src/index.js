09dc5f06aa71cbef787b9598491be5b40bfe7b82
Iryna Serhiienko <irishka.sergienko@gmail.com>
2023-03-01 20:03:33 +0200

    Added HashRouter

--- my-app1/src/index.js
+++ my-app1/src/index.js
@@ -53,77 +53,65 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import state from './redux/state';
 
-const publicUrl = process.env.PUBLIC_URL;
-const baseUrl = document.baseURI.substring(
-  0,
-  document.baseURI.indexOf('/', 10)
-);
-console.log(`publicUrl: ${publicUrl}`); //    /react
-console.log(`baseUrl: ${baseUrl}`); //    http://localhost:3000
+// const publicUrl = process.env.PUBLIC_URL;
+// const baseUrl = document.baseURI.substring(
+//   0,
+//   document.baseURI.indexOf('/', 10)
+// );
+// console.log(`publicUrl: ${publicUrl}`); //    /react
+// console.log(`baseUrl: ${baseUrl}`); //    http://localhost:3000
 
-const currentUrl = window.location.href;
-console.log(`currentUrl: ${currentUrl}`); //  http://localhost:3000/react/profile/dialogs
+// const currentUrl = window.location.href;
+// console.log(`currentUrl: ${currentUrl}`); //  http://localhost:3000/react/profile/dialogs
 
-let appUrl = publicUrl;
+// let appUrl = publicUrl;
 
-// if (currentUrl.startsWith(baseUrl + '/profile')) {
-//   appUrl = publicUrl + '/profile';
+
+// if (currentUrl.startsWith(baseUrl + `${publicUrl}/profile`)) {
+//   alert('Hi profile');
+//   appUrl = `${publicUrl}/profile`;
 // }
-if (currentUrl.startsWith(baseUrl + `${publicUrl}/profile`)) {
-  alert('Hi profile');
-  appUrl = `${publicUrl}/profile`;
-}
-// if (currentUrl.startsWith(baseUrl + '/dialogs')) {
-//   appUrl = publicUrl + '/dialogs';
+
+// if (currentUrl.startsWith(baseUrl + `${publicUrl}/dialogs`)) {
+//   alert('Hi dialogs');
+//   console.log('Hi');
+//   appUrl = `${publicUrl}/dialogs`;
 // }
-if (currentUrl.startsWith(baseUrl + `${publicUrl}/dialogs`)) {
-  alert('Hi dialogs');
-  console.log('Hi');
-  appUrl = `${publicUrl}/dialogs`;
-}
-// if (currentUrl.startsWith(baseUrl + '/news')) {
-//   appUrl = publicUrl + '/news';
+
+// if (currentUrl.startsWith(baseUrl + `${publicUrl}/news`)) {
+//   alert('Hi news');
+//   appUrl = `${publicUrl}/news`;
 // }
-if (currentUrl.startsWith(baseUrl + `${publicUrl}/news`)) {
-  alert('Hi news');
-  appUrl = `${publicUrl}/news`;
-}
-// if (currentUrl.startsWith(baseUrl + '/music')) {
-//   appUrl = publicUrl + '/music';
+
+// if (currentUrl.startsWith(baseUrl + `${publicUrl}/music`)) {
+//   alert('Hi music');
+//   appUrl = `${publicUrl}/music`;
 // }
-if (currentUrl.startsWith(baseUrl + `${publicUrl}/music`)) {
-  alert('Hi music');
-  appUrl = `${publicUrl}/music`;
-}
-// if (currentUrl.startsWith(baseUrl + '/settings')) {
-//   appUrl = publicUrl + '/settings';
+
+// if (currentUrl.startsWith(baseUrl + `${publicUrl}/settings`)) {
+//   alert('Hi settings');
+//   appUrl = `${publicUrl}/settings`;
 // }
-if (currentUrl.startsWith(baseUrl + `${publicUrl}/settings`)) {
-  alert('Hi settings');
-  appUrl = `${publicUrl}/settings`;
-}
-// if (currentUrl.startsWith(baseUrl + '/fetch')) {
-//   appUrl = publicUrl + '/fetch';
+// if (currentUrl.startsWith(baseUrl + `${publicUrl}/fetch`)) {
+//   alert('Hi fetch');
+//   appUrl = `${publicUrl}/fetch`;
 // }
-if (currentUrl.startsWith(baseUrl + `${publicUrl}/fetch`)) {
-  alert('Hi fetch');
-  appUrl = `${publicUrl}/fetch`;
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename={appUrl}>
+//   <HashRouter basename={appUrl}>
+  <HashRouter>
     <React.StrictMode>
       <App state={state} />
     </React.StrictMode>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
